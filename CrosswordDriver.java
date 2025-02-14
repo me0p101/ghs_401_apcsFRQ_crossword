408d2188d650701ed770a5d2b1274ab9558daaaa
kenon-willis <34263483+kenon-willis@users.noreply.github.com>
2025-02-14 14:57:21 -0800

    Update CrosswordDriver.java

--- CrosswordDriver.java
+++ CrosswordDriver.java
@@ -1,42 +1,18 @@
- 
+public class CrosswordDriver {
+    public static final boolean T = true;
+    public static final boolean F = false;
+    public static void main(String[] args) {
+        boolean[][] board = {
+            { T, F, F, T, T, T, F, F, F },
+            { F, F, F, F, T, F, F, F, F },
+            { F, F, F, F, F, F, T, T, T },
+            { F, F, T, F, F, F, T, F, F },
+            { T, T, T, F, F, F, F, F, F },
+            { F, F, F, F, T, F, F, F, F },
+            { F, F, F, T, T, T, F, F, T },
+        };
 
-public class CrosswordDriver
-   {
-   public static void main()
-      {
-      Crossword myPuzzle = new Crossword();
-      
-      System.out.println( myPuzzle );
-      
-      
-      
-      
-      boolean[][] board = new boolean[5][5];  
-      for(int row = 0; row < board.length; row++ )
-         {
-         for( int col = 0; col < board[0].length; col++ )
-            {
-            board[row][col] = false;    
-            } // end inner for
-         } // end outer for
-         
-         board[0][0]=true;
-         board[0][2]=true;
-         board[0][3]=true;
-         board[1][3]=true;
-         board[3][0]=true;
-         board[4][0]=true;
-         board[4][1]=true;
-         
-         
-         
-         Crossword myPuzzle2 = new Crossword(board);
-         System.out.println( myPuzzle2 );
-      
-      
-      
-      
-      
-      } // end method main
-       
-   } // end class CrosswordDriver
+        Crossword myPuzzle = new Crossword(board);
+        System.out.println(myPuzzle);
+    }
+}
